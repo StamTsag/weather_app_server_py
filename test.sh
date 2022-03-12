1672e5b77ebfda90becfbf1a693315db3a14b1cc
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-03-12 18:15:41 +0100

    smarter test script

--- test.sh
+++ test.sh
@@ -1,4 +1,15 @@
+DATA='{"y":2022,"m":3,"d":12,"h":18,"averageTemp":"22.17"}'
+URL="http://127.0.0.1:8000"
+METHOD="add_temp"
+URL_BOTH=""
+
+if [ "$1" = "last" ]; then
+  METHOD="add_last_temp"
+elif [ "$1" = "both" ]; then
+  URL_BOTH="$URL/add_last_temp"
+fi
+
 curl --header "Content-Type: application/json" \
      --request POST \
-     --data '{"y":2022,"m":3,"d":9,"h":22,"averageTemp":"22.29"}' \
-     http://127.0.0.1:8000/add_temp
+     --data $DATA \
+     "$URL/$METHOD" $URL_BOTH
